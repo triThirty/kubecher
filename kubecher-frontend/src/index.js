35f125e36a8d322782880c1e52425d644b99dcec
unknown <maoshanshi@outlook.com>
2019-12-02 17:15:40 +0800

    add frontend

---
+++ kubecher-frontend/src/index.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Square(props) {
+  return (
+      <button className="square" onClick={props.onClick}>
+          {props.value}
+      </button>
+  )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+  
+class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        current: "O",
+        history: [Array(9).fill(null)]
+      };
+    }
+
+    heandleClick(i) {
+      const squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      if( this.state.current === 'X') {
+        this.setState({current:'O'})
+      }else{
+        this.setState({current:'X'})
+      }
+      squares[i] = this.state.current;
+      const hisotry = this.state.history.slice();
+      hisotry.push(squares)
+      this.setState({history: hisotry})
+      this.setState({squares: squares});
+
+    }
+
+    renderSquare(i) {
+      return (
+        <Square 
+          value={this.state.squares[i]}
+          onClick={() => this.heandleClick(i)}
+        />
+      );
+    }
+
+    retrive(){
+      const history = this.state.history
+      const lastStep = history.pop()
+      this.setState({hisotry: hisotry})
+      
+    }
+  
+    render() {
+      // const status = `Next player: ${ this.state.current === 'O'?'O':'X'}`;
+
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = `Next player: ${ this.state.current === 'O'?'O':'X'}`;
+      }
+
+      return (
+        <div>
+          <button onClick={this.retrive}> retrive </button> 
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
